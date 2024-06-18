f70042b6a5f75610bd264a469b3a47f135fa18da
Alec Larson <ALarsonPi@gmail.com>
2024-06-18 10:39:41 -0600

    Updated a few more records on Dev

--- src/LSA-Send-Localization.ts
+++ src/LSA-Send-Localization.ts
@@ -8,7 +8,7 @@ class LoadInJSONCallEndpoint {
 
     static async loadInJSONAndCallEndpoint() {
         // Load JSON from a file. Please put json files in 'json' folder
-        const jsonFilePath = this.FILE_PREFIX + 'localization-example.json';
+        const jsonFilePath = this.FILE_PREFIX + 'stage-update-localizations.json';
         const localizations: LsaLocalization[] = await this.getLocalizationListFromJson(jsonFilePath);
 
         const DEV_HOST = 'https://api.test.lang.mtc.byu.edu/lsa/v1';
@@ -24,9 +24,9 @@ class LoadInJSONCallEndpoint {
             Find 'token' -> Payload, 
             Copy entire 'access-token') 
         */
-        const accessToken = "eyJraWQiOiJhNVwvem0zZ1pJSVVjeFZybWhKU0dvWFE3UlNTU1JoNE9wOGd5b3pHa1JwND0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI5NTc2MDgwZC1kZmJmLTRmNDYtYTY2OC1jMjQ2ZDc1NzkzOTIiLCJjb2duaXRvOmdyb3VwcyI6WyJ1cy13ZXN0LTJfQ3N5QmwxV0NoX0NodXJjaC1BY2NvdW50LU9wZW5JRCJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtd2VzdC0yLmFtYXpvbmF3cy5jb21cL3VzLXdlc3QtMl9Dc3lCbDFXQ2giLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIyOTFnc21nazc0b2g1cjRyZW5yaDBuYm4zNiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUiLCJhdXRoX3RpbWUiOjE3MTg2Njg5NDcsImV4cCI6MTcxODcyNzg5NiwiaWF0IjoxNzE4NzI0Mjk3LCJqdGkiOiJiMjhjNmZhYy1lYzBmLTQxMzEtOTBjNi0yNmY0NjEwOGJjNjgiLCJ1c2VybmFtZSI6ImNodXJjaC1hY2NvdW50LW9wZW5pZF8wMHUxd3hkZ2tvcG5keHUxYzM1NyJ9.Bu2lCMi4k5lVHC7NO75Y83xbKMt9ZyO00JKV24m-CsMOWS12O17rD7mcDSi3idY8ahPk4MfSG_mBcSA1Kl__yILXYczd7qPWRuNT0nZNgqVdUidQDYARyK9vtmEZuvrxcl5aIJO1UbZmjkIww7SlcDlPq4TJGlEN7oZMEgYuRVDLFnMf8sHOEk7o6mgMLaKE_P2St8Ijws2RWfAPpVCFXTNMxvkACS6lBxOvzH6ITt3kxBBwikNwqM1VFiStBMUPiEOSuxjIpqY_HU5cdC76uWkyOYcS1oVsCwbfjtXrzbAcYSFBk-In7-TFoxyVCEUuKtGKdh8Dd0mkUSkh1_kTKA";
+        const accessToken = "eyJraWQiOiJhNVwvem0zZ1pJSVVjeFZybWhKU0dvWFE3UlNTU1JoNE9wOGd5b3pHa1JwND0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI5NTc2MDgwZC1kZmJmLTRmNDYtYTY2OC1jMjQ2ZDc1NzkzOTIiLCJjb2duaXRvOmdyb3VwcyI6WyJ1cy13ZXN0LTJfQ3N5QmwxV0NoX0NodXJjaC1BY2NvdW50LU9wZW5JRCJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtd2VzdC0yLmFtYXpvbmF3cy5jb21cL3VzLXdlc3QtMl9Dc3lCbDFXQ2giLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIyOTFnc21nazc0b2g1cjRyZW5yaDBuYm4zNiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUiLCJhdXRoX3RpbWUiOjE3MTg2Njg5NDcsImV4cCI6MTcxODczMTUyMywiaWF0IjoxNzE4NzI3OTIzLCJqdGkiOiJjZGUyZmNiMy0yNTU5LTQ2YzQtOThhYS1kZjgzMmNjMTNiYWQiLCJ1c2VybmFtZSI6ImNodXJjaC1hY2NvdW50LW9wZW5pZF8wMHUxd3hkZ2tvcG5keHUxYzM1NyJ9.wDk3fQJa-_nO9of5Xui-XQZudFgb9pXNi1zvXTTPmdsRvF78aZtfFSA4nkHtvXsmqrEjben5z8EtK7FV_BHwqjERuoKvLXP1Vg0Lr07Ps13k9mKBPccMGJxBXkr4ZO7eijOReEpSlvFIjRqf3FP6S_4HQNrv54JO1sfAzPuKf3jQT_kJs_LeTj1BlfdfTSNpCDNq0NM0n3s1ra7_vZM5lC44Tq2VL_3YvMnpxH-Q1dvuu_MuaISXbuT2p8ZZj_rYkfn91_OvNzGGwflSJdhRhDxbw--EAeKi53RGnr1HK84L8rIFNaZycyDfn3E4bx89zO0azPSFYHkZ8nxkLWch1A";
 
-        const response = await this.makePostRequest(localizations, saveNewLocalizationsEndpoint, accessToken);
+        const response = await this.makePostRequest(localizations, updateLocalizationsEndpoint, accessToken);
         console.log("Response", response);
     }
 
